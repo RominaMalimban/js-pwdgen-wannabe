2f67cc837b83f3c5fdfe4f6d38da3d4c9422a62a
RominaMalimban <romina.lyn@hotmail.it>
2022-11-04 13:48:06 +0100

    dichiarazioni variabili nome, cog, colore

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Chiedo all’utente il suo nome
+let name = prompt ("Inserisci il tuo nome");
+console.log(name);
+
+// poi chiedo il suo cognome 
+let surname = prompt ("Inserisci il tuo cognome");
+console.log(surname);
+
+// poi chiedo il suo colore preferito
+let color = prompt ("Inserisci il tuo colore preferito");
+console.log(color);
+
+// Infine scrivo sulla pagina nome cognome colore preferito21
