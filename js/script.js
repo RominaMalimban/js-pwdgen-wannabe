66d5c955443309288fddd0dd9b200170547ce880
RominaMalimban <romina.lyn@hotmail.it>
2022-11-04 14:05:56 +0100

    aggiunta ";" a fine riga di codice

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// Chiedo all’utente il suo nome
+// Chiedo all’utente il suo nome:
 let name = prompt ("Inserisci il tuo nome");
 console.log(name);
 
-// poi chiedo il suo cognome 
+// poi chiedo il suo cognome:
 let surname = prompt ("Inserisci il tuo cognome");
 console.log(surname);
 
-// poi chiedo il suo colore preferito
+// poi chiedo il suo colore preferito:
 let color = prompt ("Inserisci il tuo colore preferito");
 console.log(color);
 
 
-// poi creo variabile di tipo numero 21
+// poi creo variabile di tipo numero 21:
 let number = 21;
-console.log(number)
+console.log(number);
 
 // Infine scrivo sulla pagina nome cognome colore preferito21
-document.getElementById("text").innerHTML= `Ciao il mio nome è ${name} e il mio cognome è ${surname}. Il mio colore preferito è ${color} ${number}.`
+document.getElementById("text").innerHTML= `Ciao il mio nome è ${name} e il mio cognome è ${surname}. Il mio colore preferito è ${color} ${number}.`;
